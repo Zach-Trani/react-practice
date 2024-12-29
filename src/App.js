30ebdb0a418bcf6b7f21cf7ee6f462203ad4f171
Zach-Trani <trani.zach@gmail.com>
2024-12-29 15:26:07 -0500

    finished child to parent component data transfer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import SearchBar from './components/SearchBar'
 
 function App() {
-  // receive some term from the child component
+  // Step 1 - Define a callback function in parent component
   const handleSubmit = (term) => {
     console.log('Do a search with', term)
     // searchImages(term);
@@ -9,6 +9,8 @@ function App() {
   
   return (
     <div>
+      {/* Step 2 - Pass the callback function as a prop to the child */}
+      {/* onSubmit can be called anything in our event handling technique with a custom component */}
       <SearchBar onSubmit={handleSubmit} />
     </div>
   );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 // instead of passing in the typical 'props' obj, we can destructure to just the onSubmit
 // function SearchBar(props){}
 
+// Can pass in the props obj, or we can destructure that obj into onSubmit.
 // prop sent: onSubmit
 // prop reveived: onSubmit
 function SearchBar({ onSubmit }) {
