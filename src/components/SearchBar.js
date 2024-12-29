af911ae90eb1d4a0ab25732aa707e138162fc69a
Zach-Trani <trani.zach@gmail.com>
2024-12-29 15:59:46 -0500

    form field changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,20 @@
 // Can pass in the props obj, or we can destructure that obj into onSubmit.
 // prop sent: onSubmit
 // prop reveived: onSubmit
-function SearchBar({ onSubmit }) {
-  const handleClick = () => {
-    onSubmit('cars')
+function SearchBar( {onSubmit} ) {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // console.log(event.target[0].value)
+    onSubmit('cars');
+
   }
 
   return (
     <div>
-      <input />
-      <button onClick={handleClick}>Click Me</button>
+      {/* pressing enter with a form and input creates a submit event */}
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
     </div>
   );
 }
